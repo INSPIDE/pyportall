b9ad85c5a2c266f6d6baa9906e765d3f30bd2f6f
Daniel Carrión <dcarrion@inspide.com>
2020-10-28 12:41:31 +0000

    update metadata

--- pyportall/metadata.py
+++ pyportall/metadata.py
@@ -1,11 +1,14 @@
-import os
-import httpx
+import logging
 from datetime import date
 from typing import Dict, List, Optional, Union
 from enum import Enum
 from pydantic.main import BaseModel
 
+
 from pyportall.api import APIClient, APIHelper
+
+
+logger = logging.getLogger("metadata")
 
 
 class Aggregate(str, Enum):
@@ -23,18 +26,37 @@ class DataType(str, Enum):
     json = "json"
 
 
+class Format(str, Enum):
+    decimal = "decimal"
+    integer = "integer"
+    percent = "percent"
+    ratio = "ratio"
+    currency = "currency"
+    date = "date"
+    timestamp = "timestamp"
+    time = "time"
+    usage= "usage"
+    text = "text"
+    none = ""
+
+
 class IndicatorMetadata(BaseModel):
     code: str
     name: str
     description: str
+    table_name: str
     unit: Optional[str] = None
-    format: Optional[str] = None
+    format: Format
     coverage: Optional[str] = None
     resolution: Optional[str] = None
     data_source: str
     computed_date: date
     aggregate_fn: Aggregate
     data_type: DataType
+    aggregate_weight: str
+    factor: float
+    immutable: bool
+    credits: int
 
 
 class MetadataHelper(APIHelper):
